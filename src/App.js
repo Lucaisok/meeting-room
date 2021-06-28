728f0b4a065dd5950a0a6a66033a74d726c939b1
Luca Tomarelli <lucatomarelli1@gmail.com>
2021-06-28 16:29:58 +0200

    calendar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+
+moment.locale("de");
+const localizer = momentLocalizer(moment);
+
+const myEventsList = [] //empty object for now
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Meeting Room
-        </p>
-      </header>
+    <div>
+      <h1>Meeting Room</h1>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   );
 }
